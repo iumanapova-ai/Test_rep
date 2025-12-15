4e42226935a8a673f92449ae92475067fb36d804
iumanapova-ai <iumanapova@yandex.ru>
2025-12-15 19:07:52 +0300

    class Summ_digit

--- Projekt_test.py
+++ Projekt_test.py
@@ -1,3 +1,13 @@
 class Summ_digit:
-    sum = 1 + 2 + 3
-    print(sum)
+
+    def __init__(self, one_digit, two_digit):
+        self.one_digit = one_digit
+        self.two_digit = two_digit
+
+    def summ_digit(self):
+        summ = self.one_digit + self.two_digit
+        print(summ)
+
+
+Summ_digit(1, 2).summ_digit()
+
