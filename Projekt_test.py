9324657e466de0cead45eef9f97d81e16937eab6
iumanapova-ai <iumanapova@yandex.ru>
2026-02-08 19:00:00 +0300

    Проверка

--- Projekt_test.py
+++ Projekt_test.py
@@ -12,15 +12,32 @@ from selenium.webdriver.common.by import By
 from time import sleep
 
 browser = webdriver.Chrome()
-sbis_site = 'https://saby.ru/'
+saby_site = 'https://saby.ru/'
+tensor_site = 'https://tensor.ru/'
+
 try:
-    browser.get(sbis_site)
+    '''Открываем сайт https://saby.ru/'''
+    browser.get(saby_site)
     sleep(1)
-    assert browser.current_url == sbis_site, 'Неверно открыт сайт'
-    start_btn = browser.find_element(By.CSS_SELECTOR, '.sbis_ru-Banner-btn')
+    assert browser.current_url == saby_site, 'Неверно открыт сайт'
+    print(f'Открыт сайт {saby_site}')
+
+    '''Переходим в раздел Контакты'''
+    start_btn = browser.find_element(By.CSS_SELECTOR, '.sbisru-Header__menu-item-1')
     start_btn.click()
-    handles = browser.window_handles
-    browser.switch_to.window(handles[1])
+    print(f'Нажали вкладку {start_btn.text}')
+    sleep(1)
+    next_btn = browser.find_element(By.CSS_SELECTOR, '[href="/contacts"]')
+    sleep(1)
+    assert next_btn.is_displayed(), 'Элемент не отображается'
+    button_txt = 'Еще 13 офисов в регионе'
+    assert next_btn.text == button_txt
+    next_btn.click()
+    print(f'Кликнули по ссылке {button_txt}')
+    assert browser.title == "Saby Контакты — Ярославская область", 'Неверно открыт сайт'
+    print(f'Заголовок страницы {browser.title}')
+    # handles = browser.window_handles
+    # browser.switch_to.window(handles[1])
 
 finally:
     browser.quit()
